7f0e0c50f9eba83ccea204d24504362b626062e2
dazay(aka dazarius_) <merlin228mordor@gmail.com>
2024-02-12 17:30:17 +0100

    4

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -32,7 +32,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
         },
       ],
       image: {
-        src: `${NEXT_PUBLIC_URL}/park-1.png`,
+        src: `${NEXT_PUBLIC_URL}/ipfs/QmTauNfnxQxVKFPvGAsG7LK5AXnMTgAUuPESiixvNubdeH`,
       },
       postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
     }),
